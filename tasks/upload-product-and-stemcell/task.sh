e45507dd6804bf0b4159750e30b473718c836cd1
Chris Mattingly <chris.mattingly@emc.com>
2018-05-23 10:24:48 -0400

    pas windows updates

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -61,7 +61,7 @@ if [[ "$PASWINDOWS" = "true" ]]; then
    cd pivnet-product
    unzip winfs-injector*.zip winfs-injector-linux
    chmod 755 winfs-injector-linux
-   ./winfs-injector-linux --input-tile ${FILE_PATH} --output-tile pas-windows-injected.pivotal
+   ./winfs-injector-linux --input-tile *.pivotal --output-tile pas-windows-injected.pivotal
    om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p pas-windows-injected.pivotal
 else
    om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
