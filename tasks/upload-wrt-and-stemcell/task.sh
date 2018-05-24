697bc0e58ec003ccc9494ba17ea5d6fc1c9e5827
Chris Mattingly <chris.mattingly@emc.com>
2018-05-24 13:36:29 -0400

    new dir for wrt

--- tasks/upload-wrt-and-stemcell/task.sh
+++ tasks/upload-wrt-and-stemcell/task.sh
@@ -41,8 +41,12 @@ if [ -n "$STEMCELL_VERSION" ]; then
 
   if [[ -z "$stemcell" ]]; then
     echo "Downloading stemcell $STEMCELL_VERSION"
-    pivnet-cli login --api-token="$PIVNET_API_TOKEN"
-    pivnet-cli download-product-files -p stemcells -r $STEMCELL_VERSION -g "*${IAAS}*" --accept-eula
+    if [[ "$PASWINDOWS" = "true" ]]; then
+       echo "S3 download of $STEMCELL_VERSION"
+    else
+       pivnet-cli login --api-token="$PIVNET_API_TOKEN"
+       pivnet-cli download-product-files -p stemcells -r $STEMCELL_VERSION -g "*${IAAS}*" --accept-eula
+    fi
 
     SC_FILE_PATH=`find ./ -name *.tgz`
 
@@ -56,7 +60,15 @@ if [ -n "$STEMCELL_VERSION" ]; then
 fi
 
 FILE_PATH=`find ./pivnet-product -name *.pivotal`
-$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
+if [[ "$PASWINDOWS" = "true" ]]; then
+   cd pivnet-product
+   unzip winfs-injector*.zip winfs-injector-linux
+   chmod 755 winfs-injector-linux
+   ./winfs-injector-linux --input-tile *.pivotal --output-tile pas-windows-injected.pivotal
+   $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p pas-windows-injected.pivotal
+else
+   $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
+fi
 
 #
 # Sleep for a while in case the problems with not staging we are seeing are because the upload hasn't really finished
